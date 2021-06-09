7d7872f8721ea90dd71f999cbfb20a5d0dcbf3b7
Bryce Adelstein Lelbach aka wash <brycelelbach@gmail.com>
2021-06-09 09:01:18 -0700

    Use an include guard instead of `#pragma once` in single file header generation.
    URL includes in Godbolt appear to be expanded into the input source directly,
    not through the C++ preprocessor, which will cause warnings if you use `#pragma
    once` in your single file header, because most compilers warn if you have
    `#pragma once` in a non-header file.

--- make_single_header.py
+++ make_single_header.py
@@ -15,7 +15,7 @@ def find_file(included_name, current_file):
             return try_path
     return None
 
-def process_file(file_path, out_lines=[], front_matter_lines=[], processed_files=[]):
+def process_file(file_path, out_lines=[], front_matter_lines=[], back_matter_lines=[], processed_files=[]):
     out_lines += "//BEGIN_FILE_INCLUDE: " + file_path + "\n"
     with open(file_path, "r") as f:
         for line in f:
@@ -26,7 +26,7 @@ def process_file(file_path, out_lines=[], front_matter_lines=[], processed_files
                 if inc_path not in processed_files:
                     if inc_path is not None:
                         processed_files += [inc_path]
-                        process_file(inc_path, out_lines, front_matter_lines, processed_files)
+                        process_file(inc_path, out_lines, front_matter_lines, back_matter_lines, processed_files)
                     else:
                         # assume it's a system header; add it to the front matter just to be clean
                         front_matter_lines += [line]
@@ -39,11 +39,14 @@ def process_file(file_path, out_lines=[], front_matter_lines=[], processed_files
             if line[-1] != "\n": line = line + "\n"
             out_lines += [line]
     out_lines += "//END_FILE_INCLUDE: " + file_path + "\n"
-    return "".join(front_matter_lines) + "\n" + "".join(out_lines)
+    return "".join(front_matter_lines) + "\n" + "".join(out_lines) + "".join(back_matter_lines)
 
 if __name__ == "__main__":
-    print(process_file(abspath(sys.argv[1]), [], ["#pragma once\n"], [abspath(sys.argv[1])]))
-
-
-
+    print(process_file(abspath(sys.argv[1]), [],
+      # We use an include guard instead of `#pragma once` because Godbolt will
+      # cause complaints about `#pragma once` when they are used in URL includes.
+      ["#ifndef _MDSPAN_SINGLE_HEADER_INCLUDE_GUARD_\n",
+       "#define _MDSPAN_SINGLE_HEADER_INCLUDE_GUARD_\n"],
+      ["#endif // _MDSPAN_SINGLE_HEADER_INCLUDE_GUARD_\n"],
+      [abspath(sys.argv[1])]))
 
