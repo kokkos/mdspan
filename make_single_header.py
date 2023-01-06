906e0369f8bdc0d18d2089383af0f7f6fc14cd46
Zongwei Lan <lanzongwei541@gmail.com>
2023-01-06 16:05:23 +0800

    Fix `make_single_header.py` to satisfy lower version compiler when use single_header version mdspan
    Format with black

--- make_single_header.py
+++ make_single_header.py
@@ -7,6 +7,7 @@ from os.path import dirname, join as path_join, abspath, exists
 
 extra_paths = [path_join(dirname(abspath(__file__)), "include")]
 
+
 def find_file(included_name, current_file):
     current_dir = dirname(abspath(current_file))
     for idir in [current_dir] + extra_paths:
@@ -15,7 +16,14 @@ def find_file(included_name, current_file):
             return try_path
     return None
 
-def process_file(file_path, out_lines=[], front_matter_lines=[], back_matter_lines=[], processed_files=[]):
+
+def process_file(
+    file_path,
+    out_lines=[],
+    front_matter_lines=[],
+    back_matter_lines=[],
+    processed_files=[],
+):
     out_lines += "//BEGIN_FILE_INCLUDE: " + file_path + "\n"
     with open(file_path, "r") as f:
         for line in f:
@@ -26,27 +34,46 @@ def process_file(file_path, out_lines=[], front_matter_lines=[], back_matter_lin
                 if inc_path not in processed_files:
                     if inc_path is not None:
                         processed_files += [inc_path]
-                        process_file(inc_path, out_lines, front_matter_lines, back_matter_lines, processed_files)
+                        process_file(
+                            inc_path,
+                            out_lines,
+                            front_matter_lines,
+                            back_matter_lines,
+                            processed_files,
+                        )
                     else:
-                        # assume it's a system header; add it to the front matter just to be clean
-                        front_matter_lines += [line]
+                        # assume it's a system header
+                        out_lines += [line]
                 continue
             m_once = re.match(r"#pragma once\s*", line)
             # ignore pragma once; we're handling it here
             if m_once:
                 continue
             # otherwise, just add the line to the output
-            if line[-1] != "\n": line = line + "\n"
+            if line[-1] != "\n":
+                line = line + "\n"
             out_lines += [line]
     out_lines += "//END_FILE_INCLUDE: " + file_path + "\n"
-    return "".join(front_matter_lines) + "\n" + "".join(out_lines) + "".join(back_matter_lines)
+    return (
+        "".join(front_matter_lines)
+        + "\n"
+        + "".join(out_lines)
+        + "".join(back_matter_lines)
+    )
+
 
 if __name__ == "__main__":
-    print(process_file(abspath(sys.argv[1]), [],
-      # We use an include guard instead of `#pragma once` because Godbolt will
-      # cause complaints about `#pragma once` when they are used in URL includes.
-      ["#ifndef _MDSPAN_SINGLE_HEADER_INCLUDE_GUARD_\n",
-       "#define _MDSPAN_SINGLE_HEADER_INCLUDE_GUARD_\n"],
-      ["#endif // _MDSPAN_SINGLE_HEADER_INCLUDE_GUARD_\n"],
-      [abspath(sys.argv[1])]))
-
+    print(
+        process_file(
+            abspath(sys.argv[1]),
+            [],
+            # We use an include guard instead of `#pragma once` because Godbolt will
+            # cause complaints about `#pragma once` when they are used in URL includes.
+            [
+                "#ifndef _MDSPAN_SINGLE_HEADER_INCLUDE_GUARD_\n",
+                "#define _MDSPAN_SINGLE_HEADER_INCLUDE_GUARD_\n",
+            ],
+            ["#endif // _MDSPAN_SINGLE_HEADER_INCLUDE_GUARD_\n"],
+            [abspath(sys.argv[1])],
+        )
+    )
