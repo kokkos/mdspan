69bcfac11aadc373c986c2424e0670922e03cd17
Mark Hoemmen <mhoemmen@nvidia.com>
2023-04-10 10:54:14 -0600

    Address some review feedback
    * Consolidate public / private declarations
    * Use bit_cast (restoring constexpr) if the compiler supports it
    * Remove unused variable

--- examples/pitched_allocation/pitched_allocation.cpp
+++ examples/pitched_allocation/pitched_allocation.cpp
@@ -1,17 +1,31 @@
 #include <experimental/mdspan>
+
+#if defined(__cpp_lib_bit_cast)
+#  include <bit>
+#endif
 #include <cassert>
 #include <cstring>
 #include <cstdint>
 #include <memory>
+#include <type_traits>
 
-// This example shows how to deal with "pitched" allocations.  These
-// are multidimensional array allocations where the size of each
-// element might not necessarily evenly divide the number of bytes per
-// "row" of the contiguous dimension.  The commented-out example below
-// uses cudaMallocPitch to allocate a 4 x 5 two-dimensional array of
-// T, where sizeof(T) is 12.  Each row (the contiguous dimension) has
+// This example shows how to deal with "pitched" allocations.
+// These are multidimensional array allocations
+// that may have extra padding bytes at the end of each "row"
+// (the contiguous mode of the array).
+// As a result, each element's size might not evenly divide
+// the number of bytes per "row" of the contiguous dimension.
+// However, each element in the row is stored in aligned fashion,
+// so that we can use reinterpret_cast instead of memcpy
+// to access the elements.
+//
+// The commented-out example below uses cudaMallocPitch
+// to allocate a 4 x 5 two-dimensional array of T,
+// where sizeof(T) is 12.  Each row (the contiguous dimension) has
 // 64 bytes.  The last 4 bytes of each row are padding that do not
 // participate in an element.
+//
+
 
 // void* ptr = nullptr;
 // size_t pitch = 0;
@@ -42,9 +56,11 @@ struct tbs {
 class const_tbs_proxy;
 class nonconst_tbs_proxy;
 
-template<class T>
+template<class T, class Enable = void>
 class const_proxy {
 private:
+  const char* p_ = nullptr;
+
   friend class const_tbs_proxy;
   constexpr const_proxy(const char* p) noexcept
     : p_(p)
@@ -56,21 +72,43 @@ public:
     // We can't do the commented-out reinterpret_cast
     // in Standard C++, because p_ might not have correct
     // alignment to point to a T.
-    //    
+    //
     //return *reinterpret_cast<const T*>(p_);
 
     T f;
     std::memcpy(&f, p_, sizeof(T));
     return f;
   }
-
-private:
-  const char* p_ = nullptr;
 };
 
+#if defined(__cpp_lib_bit_cast)
 template<class T>
+class const_proxy<T, std::enable_if_t<std::is_trivially_copyable_v<T>>> {
+private:
+  struct bitcastable {
+    char data[sizeof(T)];
+  };
+  const bitcastable* p_ = nullptr;
+
+  // reinterpret_cast means this can't be constexpr.
+  // However, the conversion operator can be.
+  friend class const_tbs_proxy;
+  const_proxy(const char* p) noexcept
+    : p_(reinterpret_cast<const bitcastable*>(p))
+  {}
+
+public:
+  constexpr operator T() const noexcept {
+    return std::bit_cast<T>(*p_);
+  }
+};
+#endif // __cpp_lib_bit_cast
+
+template<class T, class Enable = void>
 class nonconst_proxy {
 private:
+  char* p_ = nullptr;
+
   friend class nonconst_tbs_proxy;
   constexpr nonconst_proxy(char* p) noexcept
     : p_(p)
@@ -84,15 +122,40 @@ public:
   }
 
   // Not constexpr because of memcpy
-  operator T () const noexcept { 
+  operator T () const noexcept {
     T f;
     std::memcpy(&f, p_, sizeof(T));
     return f;
   }
-
-private:
-  char* p_ = nullptr;
 };
+
+#if defined(__cpp_lib_bit_cast)
+template<class T>
+class nonconst_proxy<T, std::enable_if_t<std::is_trivially_copyable_v<T>>> {
+private:
+  struct bitcastable {
+    char data[sizeof(T)];
+  };
+  bitcastable* p_ = nullptr;
+
+  // reinterpret_cast means this can't be constexpr.
+  // However, the conversion operator can be.
+  friend class nonconst_tbs_proxy;
+  nonconst_proxy(char* p) noexcept
+    : p_(reinterpret_cast<bitcastable*>(p))
+  {}
+
+public:
+  nonconst_proxy& operator=(const T& f) noexcept {
+    std::memcpy(p_, &f, sizeof(T));
+    return *this;
+  }
+
+  constexpr operator T () const noexcept {
+    return std::bit_cast<T>(*p_);
+  }
+};
+#endif // __cpp_lib_bit_cast
 
 class nonconst_tbs_proxy {
 private:
@@ -127,8 +190,8 @@ class const_tbs_proxy {
 private:
   const char* p_ = nullptr;
 
-public:    
-  constexpr const_tbs_proxy(const char* p) noexcept
+public:
+  const_tbs_proxy(const char* p) noexcept
     : p_(p), f0(p), i(p + sizeof(float)), f1(p + sizeof(float) + sizeof(int))
   {}
 
@@ -203,9 +266,8 @@ int main() {
     assert(ptr[k].f1 == 3.0);
   }
 
-  const tbs* ptr_c = ptr;
   stdex::mdspan<const tbs, stdex::extents<int, num_elements>,
-    stdex::layout_right, const_tbs_accessor> m{data.data()}; 
+    stdex::layout_right, const_tbs_accessor> m{data.data()};
   for (std::size_t k = 0; k < num_elements; ++k) {
     assert(m[k].f0 == 1.0f);
     assert(m[k].i == 2);
@@ -213,7 +275,7 @@ int main() {
   }
 
   stdex::mdspan<tbs, stdex::extents<int, num_elements>,
-    stdex::layout_right, nonconst_tbs_accessor> m_nc{data.data()}; 
+    stdex::layout_right, nonconst_tbs_accessor> m_nc{data.data()};
   for (std::size_t k = 0; k < num_elements; ++k) {
     m_nc[k].f0 = 4.0f;
     m_nc[k].i = 5;
