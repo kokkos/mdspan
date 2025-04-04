b86c4f1c6955e2ff6bdd3abec048dc1c846527a1
Thomas Applencourt <tapplencourt@anl.gov>
2025-04-04 13:55:26 -0500

    Guard SYCL printf extension

--- include/experimental/__p0009_bits/macros.hpp
+++ include/experimental/__p0009_bits/macros.hpp
@@ -118,7 +118,9 @@ MDSPAN_FUNCTION inline void default_precondition_violation_handler(const char* c
 #elif defined(MDSPAN_IMPL_HAS_SYCL)
 MDSPAN_FUNCTION inline void default_precondition_violation_handler(const char* cond, const char* file, unsigned line)
 {
+#ifdef __INTEL_LLVM_COMPILER
   sycl::ext::oneapi::experimental::printf("%s:%u: precondition failure: `%s`\n", file, line, cond);
+#endif
   assert(0);
 }
 #else
