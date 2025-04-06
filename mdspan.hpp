1d22df94b9729f44055b08e354410024f0e89e2a
dalg24 <296978+dalg24@users.noreply.github.com>
2025-04-06 15:35:02 +0000

    Update single header build.

--- mdspan.hpp
+++ mdspan.hpp
@@ -516,7 +516,13 @@ MDSPAN_FUNCTION inline void default_precondition_violation_handler(const char* c
 #elif defined(MDSPAN_IMPL_HAS_SYCL)
 MDSPAN_FUNCTION inline void default_precondition_violation_handler(const char* cond, const char* file, unsigned line)
 {
+#ifdef __INTEL_LLVM_COMPILER
   sycl::ext::oneapi::experimental::printf("%s:%u: precondition failure: `%s`\n", file, line, cond);
+#else
+  (void) cond;
+  (void) file;
+  (void) line;
+#endif
   assert(0);
 }
 #else
