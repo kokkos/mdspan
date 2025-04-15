c7d9ba9d958d8f7d6a968304d037b830de213103
Nicolas Morales <nmmoral@sandia.gov>
2025-04-15 16:10:18 -0400

    fix inadvertant preprocess ifdef that should have been if

--- tests/test_extents.cpp
+++ tests/test_extents.cpp
@@ -306,7 +306,7 @@ struct ImplicitConversionToExts<Extents,false> {
 };
 
 
-#ifdef MDSPAN_HAS_CXX_20
+#if MDSPAN_HAS_CXX_20
 TYPED_TEST(TestExtentsCompatCtors, implicit_construct_1) {
   bool exts1_convertible_exts2 =
     std::is_convertible<typename TestFixture::extents_type1,
