00bf62f5988d8c830d8944968de0b575d25e937b
Mark Hoemmen <mhoemmen@nvidia.com>
2025-05-06 15:04:30 -0600

    P3663: Test that strided_slice works with std::cw
    Also implement exposition-only __mdspan_integral_constant_like
    concept, which is useful for canonicalization and Mandates.

--- include/experimental/__p2630_bits/strided_slice.hpp
+++ include/experimental/__p2630_bits/strided_slice.hpp
@@ -18,18 +18,42 @@
 #pragma once
 
 #include "../__p0009_bits/macros.hpp"
+#if defined(MDSPAN_ENABLE_P3663)
+#  include "constant_wrapper.hpp"
+#endif
 
 #include <type_traits>
 
 namespace MDSPAN_IMPL_STANDARD_NAMESPACE {
 
-namespace {
+#if defined(MDSPAN_ENABLE_P3663)
+template<class T>
+concept __mdspan_integral_constant_like =
+  std::is_integral_v<std::remove_cvref_t<decltype(T::value)>> &&
+  ! std::is_same_v<bool, std::remove_cvref_t<decltype(T::value)>> &&
+  std::convertible_to<T, decltype(T::value)> &&
+  std::equality_comparable_with<T, decltype(T::value)> &&
+  std::bool_constant<T() == T::value>::value &&
+  std::bool_constant<static_cast<decltype(T::value)>(T()) == T::value>::value;
+#endif // MDSPAN_ENABLE_P3663
+
+namespace { // (anonymous)
   template<class T>
   struct mdspan_is_integral_constant: std::false_type {};
 
   template<class T, T val>
   struct mdspan_is_integral_constant<std::integral_constant<T,val>>: std::true_type {};
-}
+
+  template<class T>
+  constexpr bool __mdspan_is_index_like_v =
+    (std::is_integral_v<T> && ! std::is_same_v<bool, T>) ||
+#if defined(MDSPAN_ENABLE_P3663)
+    __mdspan_integral_constant_like<T>
+#else
+    mdspan_is_integral_constant<T>::value;
+#endif // MDSPAN_ENABLE_P3663
+    ;
+} // namespace (anonymous)
 
 // Slice Specifier allowing for strides and compile time extent
 template <class OffsetType, class ExtentType, class StrideType>
@@ -42,9 +66,9 @@ struct strided_slice {
   MDSPAN_IMPL_NO_UNIQUE_ADDRESS ExtentType extent{};
   MDSPAN_IMPL_NO_UNIQUE_ADDRESS StrideType stride{};
 
-  static_assert(std::is_integral_v<OffsetType> || mdspan_is_integral_constant<OffsetType>::value);
-  static_assert(std::is_integral_v<ExtentType> || mdspan_is_integral_constant<ExtentType>::value);
-  static_assert(std::is_integral_v<StrideType> || mdspan_is_integral_constant<StrideType>::value);
+  static_assert(__mdspan_is_index_like_v<OffsetType>);
+  static_assert(__mdspan_is_index_like_v<ExtentType>);
+  static_assert(__mdspan_is_index_like_v<StrideType>);
 };
 
 } // MDSPAN_IMPL_STANDARD_NAMESPACE
---
+++ tests/test_strided_slice.cpp
@@ -0,0 +1,90 @@
+//@HEADER
+// ************************************************************************
+//
+//                        Kokkos v. 4.0
+//       Copyright (2022) National Technology & Engineering
+//               Solutions of Sandia, LLC (NTESS).
+//
+// Under the terms of Contract DE-NA0003525 with NTESS,
+// the U.S. Government retains certain rights in this software.
+//
+// Part of Kokkos, under the Apache License v2.0 with LLVM Exceptions.
+// See https://kokkos.org/LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//@HEADER
+#include <mdspan/mdspan.hpp>
+#include <type_traits>
+
+#include <gtest/gtest.h>
+
+namespace {
+
+template<class OffsetType, class ExtentType, class StrideType>
+void test_strided_slice(OffsetType offset, ExtentType extent, StrideType stride)
+{
+  Kokkos::strided_slice s{offset, extent, stride};
+  static_assert(std::is_same_v<decltype(s), Kokkos::strided_slice<OffsetType, ExtentType, StrideType>>);
+  auto offset2 = s.offset;
+  static_assert(std::is_same_v<OffsetType, decltype(offset2)>);
+  auto extent2 = s.extent;
+  static_assert(std::is_same_v<ExtentType, decltype(extent2)>);
+  auto stride2 = s.stride;
+  static_assert(std::is_same_v<StrideType, decltype(stride2)>);
+
+  ASSERT_EQ(offset2, offset);
+  ASSERT_EQ(extent2, extent);
+  ASSERT_EQ(stride2, stride);
+}
+
+template<class T, T Value>
+constexpr auto IC = std::integral_constant<T, Value>{};
+
+#if defined(MDSPAN_ENABLE_P3663)
+template<std::integral T, T Value>
+  requires(! std::is_same_v<T, bool>)
+struct my_integral_constant {
+  static constexpr T value = Value;
+  constexpr operator T () const { return value; }
+  static constexpr T operator() () { return value; }
+};
+
+template<class T, T Value>
+constexpr auto IC2 = my_integral_constant<T, Value>{};
+
+static_assert(
+  std::convertible_to<
+    my_integral_constant<int, 1>,
+    decltype(my_integral_constant<int, 1>::value)>);
+
+static_assert(
+  std::equality_comparable_with<
+    my_integral_constant<int, 1>,
+    decltype(my_integral_constant<int, 1>::value)>);
+
+static_assert(
+  Kokkos::__mdspan_integral_constant_like<
+    my_integral_constant<int, 1>
+  >);
+#endif // MDSPAN_ENABLE_P3663
+
+TEST(StridedSlice, WellFormed) {
+  test_strided_slice(int(1), unsigned(10), long(3));
+  test_strided_slice((signed char)(1), (unsigned short)(10), (unsigned long long)(3));
+
+  test_strided_slice(IC<int, 1>, unsigned(10), long(3));
+  test_strided_slice(int(1), IC<unsigned, 10>, long(3));
+  test_strided_slice(int(1), unsigned(10), IC<long, 3>);
+
+#if defined(MDSPAN_ENABLE_P3663)
+  test_strided_slice(std::cw<1>, unsigned(10), long(3));
+  test_strided_slice(int(1), std::cw<unsigned(10)>, long(3));
+  test_strided_slice(int(1), unsigned(10), std::cw<long(3)>);
+
+  test_strided_slice(IC2<int, 1>, unsigned(10), long(3));
+  test_strided_slice(int(1), IC2<unsigned, 10>, long(3));
+  test_strided_slice(int(1), unsigned(10), IC2<long, 3>);
+#endif
+}
+
+} // namespace (anonymous)
