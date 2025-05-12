d76b8d694ac80aa2c387a7708b51f056f145007d
Daniel Arndt <arndtd@ornl.gov>
2025-05-12 09:46:31 -0400

    Guard for C++14

--- include/experimental/__p0009_bits/utility.hpp
+++ include/experimental/__p0009_bits/utility.hpp
@@ -47,7 +47,10 @@ constexpr bool rankwise_equal(with_rank<N>, const T1& x, const T2& y, F func)
   return match;
 }
 
-inline constexpr struct
+#if MDSPAN_HAS_CXX_17
+inline
+#endif
+constexpr struct
 {
   template <class T, class I>
   MDSPAN_INLINE_FUNCTION
@@ -57,7 +60,10 @@ inline constexpr struct
   }
 } extent;
 
-inline constexpr struct
+#if MDSPAN_HAS_CXX_17
+inline
+#endif
+constexpr struct
 {
   template <class T, class I>
   MDSPAN_INLINE_FUNCTION
@@ -167,7 +173,10 @@ tuple(Elements ...) -> tuple<Elements...>;
 #endif
 } // namespace detail
 
-inline constexpr struct mdspan_non_standard_tag {
+#if MDSPAN_HAS_CXX_17
+inline
+#endif
+constexpr struct mdspan_non_standard_tag {
 } mdspan_non_standard;
 
 } // namespace MDSPAN_IMPL_STANDARD_NAMESPACE
