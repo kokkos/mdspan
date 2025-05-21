825e659680eb4a59ec0e28966a435e1da13521ca
Mark Hoemmen <mhoemmen@nvidia.com>
2025-05-21 16:49:26 -0600

    Put dims in the correct namespace

--- tests/test_dims.cpp
+++ tests/test_dims.cpp
@@ -31,7 +31,7 @@ static constexpr bool is_extents_v<
 template<std::size_t Rank>
 void test_dims_with_one_template_argument()
 {
-  using d = MDSPAN_IMPL_STANDARD_NAMESPACE :: MDSPAN_IMPL_PROPOSED_NAMESPACE :: dims<Rank>;
+  using d = MDSPAN_IMPL_STANDARD_NAMESPACE :: dims<Rank>;
   static_assert(test::is_extents_v<d>, "dims<Rank> is not an extents specialization");
   static_assert(std::is_same<typename d::index_type, std::size_t>::value, "dims::index_type is wrong");
   static_assert(d::rank() == Rank, "dims::rank() is wrong");
@@ -40,7 +40,7 @@ void test_dims_with_one_template_argument()
 template<std::size_t Rank, class ExpectedIndexType>
 void test_dims_with_two_template_arguments()
 {
-  using d = MDSPAN_IMPL_STANDARD_NAMESPACE :: MDSPAN_IMPL_PROPOSED_NAMESPACE :: dims<Rank, ExpectedIndexType>;
+  using d = MDSPAN_IMPL_STANDARD_NAMESPACE :: dims<Rank, ExpectedIndexType>;
   static_assert(test::is_extents_v<d>, "dims<Rank,T> is not an extents specialization");
   static_assert(std::is_same<typename d::index_type, ExpectedIndexType>::value, "dims::index_type is wrong");
   static_assert(d::rank() == Rank, "dims::rank() is wrong");
