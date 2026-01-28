d5ff70b6d6e510ba76be1f2bad58ea0d8c369b59
Nicolas Morales <nmmoral@sandia.gov>
2026-01-28 16:34:50 -0500

    #412: update aligned accessor example to use library aligned accessor

--- examples/aligned_accessor/aligned_accessor.cpp
+++ examples/aligned_accessor/aligned_accessor.cpp
@@ -34,6 +34,10 @@
 // https://github.com/rapidsai/raft/pull/725#discussion_r937991701
 
 namespace {
+using Kokkos::aligned_accessor;
+using Kokkos::detail::aligned_pointer_t;
+using Kokkos::detail::assume_aligned_method;
+using Kokkos::detail::align_attribute_method;
 
 using test_value_type = float;
 constexpr std::size_t min_overalignment_factor = 8;
@@ -43,128 +47,12 @@ constexpr std::size_t min_byte_alignment = min_overalignment_factor * sizeof(flo
 // Some compilers have trouble optimizing loops with unsigned or 64-bit index types.
 using index_type = int;
 
-
-// Prefer std::assume_aligned if available, as it is in the C++ Standard.
-// Otherwise, use a compiler-specific equivalent if available.
-
-// NOTE (mfh 2022/08/08) BYTE_ALIGNMENT must be unsigned and a power of 2.
-#if defined(__cpp_lib_assume_aligned)
-#  define MDSPAN_IMPL_ASSUME_ALIGNED( ELEMENT_TYPE, POINTER, BYTE_ALIGNMENT ) (std::assume_aligned< BYTE_ALIGNMENT >( POINTER ))
-  constexpr char assume_aligned_method[] = "std::assume_aligned";
-#elif defined(__ICL)
-#  define MDSPAN_IMPL_ASSUME_ALIGNED( ELEMENT_TYPE, POINTER, BYTE_ALIGNMENT ) POINTER
-  constexpr char assume_aligned_method[] = "(none)";
-#elif defined(__ICC)
-#  define MDSPAN_IMPL_ASSUME_ALIGNED( ELEMENT_TYPE, POINTER, BYTE_ALIGNMENT ) POINTER
-  constexpr char assume_aligned_method[] = "(none)";
-#elif defined(__clang__)
-#  define MDSPAN_IMPL_ASSUME_ALIGNED( ELEMENT_TYPE, POINTER, BYTE_ALIGNMENT ) POINTER
-  constexpr char assume_aligned_method[] = "(none)";
-#elif defined(__GNUC__)
-  // __builtin_assume_aligned returns void*
-#  define MDSPAN_IMPL_ASSUME_ALIGNED( ELEMENT_TYPE, POINTER, BYTE_ALIGNMENT ) reinterpret_cast< ELEMENT_TYPE* >(__builtin_assume_aligned( POINTER, BYTE_ALIGNMENT ))
-  constexpr char assume_aligned_method[] = "__builtin_assume_aligned";
-#else
-#  define MDSPAN_IMPL_ASSUME_ALIGNED( ELEMENT_TYPE, POINTER, BYTE_ALIGNMENT ) POINTER
-  constexpr char assume_aligned_method[] = "(none)";
-#endif
-
-// Some compilers other than Clang or GCC like to define __clang__ or __GNUC__.
-// Thus, we order the tests from most to least specific.
-#if defined(__ICL)
-#  define MDSPAN_IMPL_ALIGN_VALUE_ATTRIBUTE( BYTE_ALIGNMENT ) __declspec(align_value( BYTE_ALIGNMENT ))
-  constexpr char align_attribute_method[] = "__declspec(align_value(BYTE_ALIGNMENT))";
-#elif defined(__ICC)
-#  define MDSPAN_IMPL_ALIGN_VALUE_ATTRIBUTE( BYTE_ALIGNMENT ) __attribute__((align_value( BYTE_ALIGNMENT )))
-  constexpr char align_attribute_method[] = "__attribute__((align_value(BYTE_ALIGNMENT)))";
-#elif defined(__clang__)
-#  define MDSPAN_IMPL_ALIGN_VALUE_ATTRIBUTE( BYTE_ALIGNMENT ) __attribute__((align_value( BYTE_ALIGNMENT )))
-  constexpr char align_attribute_method[] = "__attribute__((align_value(BYTE_ALIGNMENT)))";
-#else
-#  define MDSPAN_IMPL_ALIGN_VALUE_ATTRIBUTE( BYTE_ALIGNMENT )
-  constexpr char align_attribute_method[] = "(none)";
-#endif
-
-constexpr bool
-is_nonzero_power_of_two(const std::size_t x)
-{
-// Just checking __cpp_lib_int_pow2 isn't enough for some GCC versions.
-// The <bit> header exists, but std::has_single_bit does not.
-#if defined(__cpp_lib_int_pow2) && __cplusplus >= 202002L
-  return std::has_single_bit(x);
-#else
-  return x != 0 && (x & (x - 1)) == 0;
-#endif
-}
-
-template<class ElementType>
-constexpr bool
-valid_byte_alignment(const std::size_t byte_alignment)
-{
-  return is_nonzero_power_of_two(byte_alignment) && byte_alignment >= alignof(ElementType);
-}
-
-// We define aligned_pointer_t through a struct
-// so we can check whether the byte alignment is valid.
-// This makes it impossible to use the alias
-// with an invalid byte alignment.
-template<class ElementType, std::size_t byte_alignment>
-struct aligned_pointer {
-  static_assert(valid_byte_alignment<ElementType>(byte_alignment),
-		"byte_alignment must be a power of two no less than "
-		"the minimum required alignment of ElementType.");
-
-#if defined(__ICC)
-  // x86-64 ICC 2021.5.0 emits warning #3186 ("expected typedef declaration") here.
-  // No other compiler (including Clang, which has a similar type attribute) has this issue.
-#  pragma warning push
-#  pragma warning disable 3186
-#endif
-
-  using type = ElementType* MDSPAN_IMPL_ALIGN_VALUE_ATTRIBUTE( byte_alignment );
-
-#if defined(__ICC)
-#  pragma warning pop
-#endif
-};
-
-template<class ElementType, std::size_t byte_alignment>
-using aligned_pointer_t = typename aligned_pointer<ElementType, byte_alignment>::type;
-
 template<class ElementType, std::size_t byte_alignment>
 aligned_pointer_t<ElementType, byte_alignment>
 bless(ElementType* ptr, std::integral_constant<std::size_t, byte_alignment> /* ba */ )
 {
   return MDSPAN_IMPL_ASSUME_ALIGNED( ElementType, ptr, byte_alignment );
 }
-
-template<class ElementType, std::size_t byte_alignment>
-struct aligned_accessor {
-  using offset_policy = Kokkos::default_accessor<ElementType>;
-  using element_type = ElementType;
-  using reference = ElementType&;
-  using data_handle_type = aligned_pointer_t<ElementType, byte_alignment>;
-
-  constexpr aligned_accessor() noexcept = default;
-
-  MDSPAN_TEMPLATE_REQUIRES(
-    class OtherElementType,
-    std::size_t other_byte_alignment,
-    /* requires */ (std::is_convertible<OtherElementType(*)[], element_type(*)[]>::value && other_byte_alignment == byte_alignment)
-    )
-  constexpr aligned_accessor(aligned_accessor<OtherElementType, other_byte_alignment>) noexcept {}
-
-  constexpr reference access(data_handle_type p, size_t i) const noexcept {
-    // This may declare alignment twice, depending on
-    // if we have an attribute for marking pointer types.
-    return MDSPAN_IMPL_ASSUME_ALIGNED( ElementType, p, byte_alignment )[i];
-  }
-
-  constexpr typename offset_policy::data_handle_type
-  offset(data_handle_type p, size_t i) const noexcept {
-    return p + i;
-  }
-};
 
 template<class ElementType>
 struct delete_raw {
