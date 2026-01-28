19d518482706b5d834a4c31f039a8dcd6c1e8345
Nicolas Morales <nmmoral@sandia.gov>
2026-01-28 16:21:33 -0500

    #412: add tests for aligned accessor

--- include/experimental/__p0009_bits/aligned_accessor.hpp
+++ include/experimental/__p0009_bits/aligned_accessor.hpp
@@ -57,8 +57,13 @@
 #include <memory>
 #include <type_traits>
 
+// If we don't have bitcast, we should use memcpy
+#ifndef __cpp_lib_bit_cast
+#include <cstring>
+#endif
+
 namespace MDSPAN_IMPL_STANDARD_NAMESPACE {
-namespace MDSPAN_IMPL_PROPOSED_NAMESPACE {
+namespace detail {
 
 // Prefer std::assume_aligned if available, as it is in the C++ Standard.
 // Otherwise, use a compiler-specific equivalent if available.
@@ -68,36 +73,36 @@ namespace MDSPAN_IMPL_PROPOSED_NAMESPACE {
 #  define MDSPAN_IMPL_ASSUME_ALIGNED( ELEMENT_TYPE, POINTER, BYTE_ALIGNMENT ) (std::assume_aligned< BYTE_ALIGNMENT >( POINTER ))
   constexpr char assume_aligned_method[] = "std::assume_aligned";
 #elif defined(__ICL)
-#  define _MDSPAN_ASSUME_ALIGNED( ELEMENT_TYPE, POINTER, BYTE_ALIGNMENT ) POINTER
+#  define MDSPAN_IMPL_ASSUME_ALIGNED( ELEMENT_TYPE, POINTER, BYTE_ALIGNMENT ) POINTER
   constexpr char assume_aligned_method[] = "(none)";
 #elif defined(__ICC)
-#  define _MDSPAN_ASSUME_ALIGNED( ELEMENT_TYPE, POINTER, BYTE_ALIGNMENT ) POINTER
+#  define MDSPAN_IMPL_ASSUME_ALIGNED( ELEMENT_TYPE, POINTER, BYTE_ALIGNMENT ) POINTER
   constexpr char assume_aligned_method[] = "(none)";
 #elif defined(__clang__)
-#  define _MDSPAN_ASSUME_ALIGNED( ELEMENT_TYPE, POINTER, BYTE_ALIGNMENT ) POINTER
+#  define MDSPAN_IMPL_ASSUME_ALIGNED( ELEMENT_TYPE, POINTER, BYTE_ALIGNMENT ) POINTER
   constexpr char assume_aligned_method[] = "(none)";
 #elif defined(__GNUC__)
   // __builtin_assume_aligned returns void*
-#  define _MDSPAN_ASSUME_ALIGNED( ELEMENT_TYPE, POINTER, BYTE_ALIGNMENT ) reinterpret_cast< ELEMENT_TYPE* >(__builtin_assume_aligned( POINTER, BYTE_ALIGNMENT ))
+#  define MDSPAN_IMPL_ASSUME_ALIGNED( ELEMENT_TYPE, POINTER, BYTE_ALIGNMENT ) reinterpret_cast< ELEMENT_TYPE* >(__builtin_assume_aligned( POINTER, BYTE_ALIGNMENT ))
   constexpr char assume_aligned_method[] = "__builtin_assume_aligned";
 #else
-#  define _MDSPAN_ASSUME_ALIGNED( ELEMENT_TYPE, POINTER, BYTE_ALIGNMENT ) POINTER
+#  define MDSPAN_IMPL_ASSUME_ALIGNED( ELEMENT_TYPE, POINTER, BYTE_ALIGNMENT ) POINTER
   constexpr char assume_aligned_method[] = "(none)";
 #endif
 
 // Some compilers other than Clang or GCC like to define __clang__ or __GNUC__.
 // Thus, we order the tests from most to least specific.
 #if defined(__ICL)
-#  define _MDSPAN_ALIGN_VALUE_ATTRIBUTE( BYTE_ALIGNMENT ) __declspec(align_value( BYTE_ALIGNMENT ));
+#  define MDSPAN_IMPL_ALIGN_VALUE_ATTRIBUTE( BYTE_ALIGNMENT ) __declspec(align_value( BYTE_ALIGNMENT ));
   constexpr char align_attribute_method[] = "__declspec(align_value(BYTE_ALIGNMENT))";
 #elif defined(__ICC)
-#  define _MDSPAN_ALIGN_VALUE_ATTRIBUTE( BYTE_ALIGNMENT ) __attribute__((align_value( BYTE_ALIGNMENT )));
+#  define MDSPAN_IMPL_ALIGN_VALUE_ATTRIBUTE( BYTE_ALIGNMENT ) __attribute__((align_value( BYTE_ALIGNMENT )));
   constexpr char align_attribute_method[] = "__attribute__((align_value(BYTE_ALIGNMENT)))";
 #elif defined(__clang__)
-#  define _MDSPAN_ALIGN_VALUE_ATTRIBUTE( BYTE_ALIGNMENT ) __attribute__((align_value( BYTE_ALIGNMENT )));
+#  define MDSPAN_IMPL_ALIGN_VALUE_ATTRIBUTE( BYTE_ALIGNMENT ) __attribute__((align_value( BYTE_ALIGNMENT )));
   constexpr char align_attribute_method[] = "__attribute__((align_value(BYTE_ALIGNMENT)))";
 #else
-#  define _MDSPAN_ALIGN_VALUE_ATTRIBUTE( BYTE_ALIGNMENT )
+#  define MDSPAN_IMPL_ALIGN_VALUE_ATTRIBUTE( BYTE_ALIGNMENT )
   constexpr char align_attribute_method[] = "(none)";
 #endif
 
@@ -124,32 +129,47 @@ valid_byte_alignment(const std::size_t byte_alignment)
 // so we can check whether the byte alignment is valid.
 // This makes it impossible to use the alias
 // with an invalid byte alignment.
-template<class ElementType, std::size_t byte_alignment>
+template<class T, std::size_t Alignment>
 struct aligned_pointer {
-  static_assert(valid_byte_alignment<ElementType>(byte_alignment),
-		"byte_alignment must be a power of two no less than "
-		"the minimum required alignment of ElementType.");
-  using type = ElementType* _MDSPAN_ALIGN_VALUE_ATTRIBUTE( byte_alignment );
+  static_assert(valid_byte_alignment<T>(Alignment),
+		"Alignment must be a power of two no less than "
+		"the minimum required alignment of T.");
+  using type = T* MDSPAN_IMPL_ALIGN_VALUE_ATTRIBUTE( Alignment );
 };
 
 
 template<class ElementType, std::size_t byte_alignment>
 using aligned_pointer_t = typename aligned_pointer<ElementType, byte_alignment>::type;
+} // namespace detail
 
-template<class ElementType, std::size_t byte_alignment>
-aligned_pointer_t<ElementType, byte_alignment>
-bless(ElementType* ptr, std::integral_constant<std::size_t, byte_alignment> /* ba */ )
-{
-  return _MDSPAN_ASSUME_ALIGNED( ElementType, ptr, byte_alignment );
+template<size_t Alignment, class T>
+#ifdef __cpp_lib_bit_cast // Only can be constexpr if we have bit_cast
+constexpr
+#endif
+bool is_sufficiently_aligned(T* ptr) {
+  // Note this mandate is not what is currently in the standard
+  // See https://cplusplus.github.io/LWG/issue4290
+  static_assert(detail::valid_byte_alignment<T>(Alignment),
+		"Alignment must be a power of two no less than "
+		"the minimum required alignment of T.");
+#ifdef __cpp_lib_bit_cast
+  auto dst = std::bit_cast<std::uintptr_t>(ptr);
+#else
+  // Will work but non-constexpr
+  std::uintptr_t dst;
+  std::memcpy(&dst, &ptr, sizeof(std::uintptr_t));
+#endif
+  return !(dst & (Alignment - 1));
 }
 
-
-template<class ElementType, std::size_t byte_alignment>
+template<class ElementType, std::size_t ByteAlignment>
 struct aligned_accessor {
-  using offset_policy = MDSPAN_IMPL_STANDARD_NAMESPACE::default_accessor<ElementType>;
+  using offset_policy = default_accessor<ElementType>;
   using element_type = ElementType;
   using reference = ElementType&;
-  using data_handle_type = aligned_pointer_t<ElementType, byte_alignment>;
+  using data_handle_type = detail::aligned_pointer_t<ElementType, ByteAlignment>;
+
+  static constexpr size_t byte_alignment = ByteAlignment;
 
   constexpr aligned_accessor() noexcept = default;
 
@@ -158,12 +178,26 @@ struct aligned_accessor {
     std::size_t OtherByteAlignment,
     /* requires */ (std::is_convertible<OtherElementType(*)[], element_type(*)[]>::value && OtherByteAlignment >= byte_alignment)
     )
-  constexpr aligned_accessor(aligned_accessor<OtherElementType, other_byte_alignment>) noexcept {}
+  constexpr aligned_accessor(aligned_accessor<OtherElementType, OtherByteAlignment>) noexcept {}
+
+  MDSPAN_TEMPLATE_REQUIRES(
+    class OtherElementType,
+    /* requires */ (std::is_convertible<OtherElementType(*)[], element_type(*)[]>::value)
+  )
+  constexpr explicit aligned_accessor(default_accessor<OtherElementType>) noexcept {}
+
+  MDSPAN_TEMPLATE_REQUIRES(
+    class OtherElementType,
+    /* requires */ (std::is_convertible<element_type(*)[], OtherElementType(*)[]>::value)
+  )
+  constexpr operator default_accessor<OtherElementType>() const noexcept {
+    return {};
+  }
 
   constexpr reference access(data_handle_type p, size_t i) const noexcept {
     // This may declare alignment twice, depending on
     // if we have an attribute for marking pointer types.
-    return _MDSPAN_ASSUME_ALIGNED( ElementType, p, byte_alignment )[i];
+    return MDSPAN_IMPL_ASSUME_ALIGNED( ElementType, p, byte_alignment )[i];
   }
 
   constexpr typename offset_policy::data_handle_type
@@ -172,6 +206,4 @@ struct aligned_accessor {
   }
 };
 
-
-}  // namespace MDSPAN_IMPL_PROPOSED_NAMESPACE
 }  // namespace MDSPAN_IMPL_STANDARD_NAMESPACE
---
+++ tests/test_aligned_accessor.cpp
@@ -0,0 +1,72 @@
+
+#include <gtest/gtest.h>
+#include <mdspan/mdspan.hpp>
+#include <numeric>
+
+TEST(TestAlignedAccessor, IsSufficientlyAligned) {
+  ASSERT_TRUE(
+      Kokkos::is_sufficiently_aligned<1>(reinterpret_cast<char *>(0x12345678)));
+  ASSERT_TRUE(
+      Kokkos::is_sufficiently_aligned<1>(reinterpret_cast<char *>(0x12345671)));
+
+  ASSERT_TRUE(Kokkos::is_sufficiently_aligned<2>(
+      reinterpret_cast<std::int16_t *>(0x12345678)));
+  ASSERT_TRUE(!Kokkos::is_sufficiently_aligned<2>(
+      reinterpret_cast<std::int16_t *>(0x12345671)));
+
+  ASSERT_TRUE(Kokkos::is_sufficiently_aligned<4>(
+      reinterpret_cast<std::int32_t *>(0x12345678)));
+  ASSERT_TRUE(Kokkos::is_sufficiently_aligned<4>(
+      reinterpret_cast<std::int32_t *>(0x12345674)));
+  ASSERT_TRUE(Kokkos::is_sufficiently_aligned<4>(
+      reinterpret_cast<std::int32_t *>(0x1234567c)));
+  ASSERT_TRUE(!Kokkos::is_sufficiently_aligned<4>(
+      reinterpret_cast<std::int32_t *>(0x12345672)));
+  ASSERT_TRUE(!Kokkos::is_sufficiently_aligned<4>(
+      reinterpret_cast<std::int32_t *>(0x12345671)));
+  ASSERT_TRUE(!Kokkos::is_sufficiently_aligned<4>(
+      reinterpret_cast<std::int32_t *>(0x12345677)));
+}
+
+template <std::size_t ByteAlignment, std::size_t NumElements>
+void test_aligned_accessor() {
+  using mdspan_type =
+      Kokkos::mdspan<double, Kokkos::extents<std::size_t, NumElements>,
+                     Kokkos::layout_right,
+                     Kokkos::aligned_accessor<double, ByteAlignment>>;
+  auto *buff = reinterpret_cast<double *>(
+      std::aligned_alloc(ByteAlignment, NumElements * sizeof(double)));
+  std::iota(buff, buff + NumElements, 0);
+  ASSERT_TRUE(Kokkos::is_sufficiently_aligned<ByteAlignment>(buff));
+
+  auto md = mdspan_type(buff);
+
+  for (std::size_t i = 0; i < NumElements; ++i)
+    ASSERT_TRUE(md[i] == static_cast<double>(i));
+
+  // Accessor should be convertible to a default one
+  auto md2 = Kokkos::mdspan<double, Kokkos::extents<std::size_t, NumElements>>(md);
+  ASSERT_TRUE(Kokkos::is_sufficiently_aligned<ByteAlignment>(md2.data_handle()));
+
+  for (std::size_t i = 0; i < NumElements; ++i)
+    ASSERT_TRUE(md2[i] == static_cast<double>(i));
+
+  // Get an offset for the submdspan that should be of the correct alignment
+  const std::size_t offset = ByteAlignment / sizeof(double);
+  static_assert( NumElements > 2 + offset );
+  auto md3 = Kokkos::submdspan(md, std::pair{ offset, NumElements - 2 } );
+  auto *ptr = md3.data_handle();
+  ASSERT_TRUE(Kokkos::is_sufficiently_aligned<ByteAlignment>(md3.data_handle()));
+
+  for (std::size_t i = 0; i < NumElements - 2 - offset; ++i )
+    ASSERT_TRUE(md3[i] == static_cast<double>(i + offset));
+
+  std::free(buff);
+}
+
+TEST(TestAlignedAccessor, AlignedAccessor) {
+  test_aligned_accessor<alignof(double), 10>();
+  test_aligned_accessor<alignof(double), 10>();
+  test_aligned_accessor<2 * alignof(double), 10>();
+  test_aligned_accessor<4 * alignof(double), 10>();
+}
