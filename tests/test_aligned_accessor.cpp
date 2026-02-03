f827aad994160c5eeadb892eb29227de685ab48f
Nicolas Morales <nmmoral@sandia.gov>
2026-02-03 10:25:30 -0500

    #412: add test with structure type whose size is different from alignment

--- tests/test_aligned_accessor.cpp
+++ tests/test_aligned_accessor.cpp
@@ -48,46 +48,67 @@ namespace testing
     std::free(ptr);
   #endif
   }
+
+  struct different_align_and_size
+  {
+    different_align_and_size() = default;
+    explicit different_align_and_size(int v) : val(static_cast<double>(v)) {}
+
+    int padd;
+    double val;
+
+    friend bool operator==(different_align_and_size lhs, different_align_and_size rhs) {
+      return lhs.val == rhs.val;
+    }
+
+    different_align_and_size &operator++() noexcept {
+      ++val;
+      return *this;
+    }
+  };
 }
 
-template <std::size_t ByteAlignment, std::size_t NumElements>
+template <typename T, std::size_t ByteAlignment, std::size_t NumElements>
 void test_aligned_accessor() {
   using mdspan_type =
-      Kokkos::mdspan<double, Kokkos::extents<std::size_t, NumElements>,
+      Kokkos::mdspan<T, Kokkos::extents<std::size_t, NumElements>,
                      Kokkos::layout_right,
-                     Kokkos::aligned_accessor<double, ByteAlignment>>;
-  auto *buff = reinterpret_cast<double *>(
-      testing::aligned_alloc(ByteAlignment, NumElements * sizeof(double)));
-  std::iota(buff, buff + NumElements, 0);
+                     Kokkos::aligned_accessor<T, ByteAlignment>>;
+  auto *buff = reinterpret_cast<T *>(
+      testing::aligned_alloc(ByteAlignment, NumElements * sizeof(T)));
+  std::iota(buff, buff + NumElements, static_cast<T>(0));
   ASSERT_TRUE(Kokkos::is_sufficiently_aligned<ByteAlignment>(buff));
 
   auto md = mdspan_type(buff);
 
   for (std::size_t i = 0; i < NumElements; ++i)
-    ASSERT_TRUE(md[i] == static_cast<double>(i));
+    ASSERT_TRUE(md[i] == static_cast<T>(i));
 
   // Accessor should be convertible to a default one
-  auto md2 = Kokkos::mdspan<double, Kokkos::extents<std::size_t, NumElements>>(md);
+  auto md2 = Kokkos::mdspan<T, Kokkos::extents<std::size_t, NumElements>>(md);
   ASSERT_TRUE(Kokkos::is_sufficiently_aligned<ByteAlignment>(md2.data_handle()));
 
   for (std::size_t i = 0; i < NumElements; ++i)
-    ASSERT_TRUE(md2[i] == static_cast<double>(i));
+    ASSERT_TRUE(md2[i] == static_cast<T>(i));
 
   // Get an offset for the submdspan that should be of the correct alignment
-  const std::size_t offset = ByteAlignment / sizeof(double);
+  const std::size_t offset = ByteAlignment / sizeof(T);
   static_assert( NumElements > 2 + offset );
   auto md3 = Kokkos::submdspan(md, std::pair{ offset, NumElements - 2 } );
   ASSERT_TRUE(Kokkos::is_sufficiently_aligned<ByteAlignment>(md3.data_handle()));
 
   for (std::size_t i = 0; i < NumElements - 2 - offset; ++i )
-    ASSERT_TRUE(md3[i] == static_cast<double>(i + offset));
+    ASSERT_TRUE(md3[i] == static_cast<T>(i + offset));
 
   testing::aligned_free(buff);
 }
 
 TEST(TestAlignedAccessor, AlignedAccessor) {
-  test_aligned_accessor<alignof(double), 10>();
-  test_aligned_accessor<alignof(double), 10>();
-  test_aligned_accessor<2 * alignof(double), 10>();
-  test_aligned_accessor<4 * alignof(double), 10>();
+  test_aligned_accessor<double, alignof(double), 10>();
+  test_aligned_accessor<double, 2 * alignof(double), 10>();
+  test_aligned_accessor<double, 4 * alignof(double), 10>();
+
+  test_aligned_accessor<testing::different_align_and_size, alignof(testing::different_align_and_size), 10>();
+  test_aligned_accessor<testing::different_align_and_size, 2 * alignof(testing::different_align_and_size), 10>();
+  test_aligned_accessor<testing::different_align_and_size, 4 * alignof(testing::different_align_and_size), 10>();
 }
