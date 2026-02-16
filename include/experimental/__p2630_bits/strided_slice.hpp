345dda4819b64e26e27ff78839fbb7896dd6020a
Mark Hoemmen <mhoemmen@nvidia.com>
2026-02-15 17:49:19 -0700

    Fix __mdspan_is_index_like_v
    For both P3663 and no-P3663 branches, fix __mdspan_is_index_like_v
    so that it now correctly excludes std::bool_constant or any
    integral-constant-like with bool value type.

--- include/experimental/__p2630_bits/strided_slice.hpp
+++ include/experimental/__p2630_bits/strided_slice.hpp
@@ -36,10 +36,26 @@ namespace detail {
   constexpr bool is_constant_wrapper<constant_wrapper<Value, Type>> = true;  
 #endif // MDSPAN_ENABLE_P3663
 
+  template<class T, class = void>
+  struct is_signed_or_unsigned_integral_constant_like : std::false_type {};
+
+  template<class T>
+  struct is_signed_or_unsigned_integral_constant_like<
+    T, std::enable_if_t<is_integral_constant_like_v<T>>
+  > : std::bool_constant<
+      std::is_integral_v<remove_cvref_t<decltype(T::value)>> &&
+      ! std::is_same_v<bool, remove_cvref_t<decltype(T::value)>>
+    >
+  {};
+
+  template<class T>
+  constexpr bool is_signed_or_unsigned_integral_constant_like_v =
+    is_signed_or_unsigned_integral_constant_like<T>::value;
+  
   template<class T>
   constexpr bool __mdspan_is_index_like_v =
     (std::is_integral_v<T> && ! std::is_same_v<bool, T>) ||
-    is_integral_constant_like_v<T>;
+    is_signed_or_unsigned_integral_constant_like_v<T>;
 } // namespace detail
 
 // Slice Specifier allowing for strides and compile time extent
