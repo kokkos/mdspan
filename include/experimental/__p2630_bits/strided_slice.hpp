f2188f5ffd26ebd2f33aa9b7dee91ffd02d3cd0a
Christian Trott <crtrott@sandia.gov>
2026-02-20 13:08:20 -0700

    Apply suggestions from code review

--- include/experimental/__p2630_bits/strided_slice.hpp
+++ include/experimental/__p2630_bits/strided_slice.hpp
@@ -43,7 +43,7 @@ namespace detail {
     is_signed_or_unsigned_integral_constant_like<T>::value;
 
   template<class T>
-  constexpr bool __mdspan_is_index_like_v =
+  constexpr bool mdspan_is_index_like_v =
     (std::is_integral_v<T> && ! std::is_same_v<bool, T>) ||
     is_signed_or_unsigned_integral_constant_like_v<T>;
 } // namespace detail
@@ -59,9 +59,9 @@ struct strided_slice {
   MDSPAN_IMPL_NO_UNIQUE_ADDRESS ExtentType extent{};
   MDSPAN_IMPL_NO_UNIQUE_ADDRESS StrideType stride{};
 
-  static_assert(detail::__mdspan_is_index_like_v<OffsetType>);
-  static_assert(detail::__mdspan_is_index_like_v<ExtentType>);
-  static_assert(detail::__mdspan_is_index_like_v<StrideType>);
+  static_assert(detail::mdspan_is_index_like_v<OffsetType>);
+  static_assert(detail::mdspan_is_index_like_v<ExtentType>);
+  static_assert(detail::mdspan_is_index_like_v<StrideType>);
 };
 
 } // MDSPAN_IMPL_STANDARD_NAMESPACE
